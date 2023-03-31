df680dc596b14e93a17d0f0f5e6b2ef83352e6f0
hossainshakil242 <realshakil8@gmail.com>
2023-03-31 17:22:03 +0600

    some file add . until to handle addtocart button . module number 50

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Product.css';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    // console.log(props);
+    const { img, name, seller, ratings, quantity, price, } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="Product-img" />
+            <div className="product-info">
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: $ {price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings}</p>
+            </div>
+            <button className='btn-cart' onClick={() => handleAddToCart(props.product)}>Add To Cart
+            <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
+        </div>
+    );
+};
+
+export default Product;
