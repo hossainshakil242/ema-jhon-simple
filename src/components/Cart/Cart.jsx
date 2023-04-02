4797f840ef003e49629cf9ce63e36ad6021b2578
hossainshakil242 <realshakil8@gmail.com>
2023-04-02 10:38:05 +0600

    add summary site and calculator

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({cart}) => {
+    //const cart = props.cart; option-1
+    //const {cart} = props;
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice + totalShipping + tax;
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Select Item: {cart.length}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
